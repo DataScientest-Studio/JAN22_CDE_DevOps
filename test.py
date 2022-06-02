3456ba1c3b11bcd1fa8735171f2b1882126500d2
Skypeblue <fredericfrancine@gmail.com>
2022-06-02 13:41:23 +0200

    correction

--- test.py
+++ test.py
@@ -13,14 +13,13 @@ def test_test():
     assert r.status_code==200
     assert "test"==r.json()
 
-
 def test_post():
     r = requests.post("http://localhost:8000{}".format("/post_test"))
     assert r.status_code==200
     assert 25==r.json()
 
 def test_health():
-    r = requests.post("http://localhost:8000{}".format("/health"))
+    r = requests.get("http://localhost:8000{}".format("/health"))
     assert r.status_code==200
     assert "health" in r.json()
 
